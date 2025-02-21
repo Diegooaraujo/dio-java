939fa97d1f530c4d0fca1546cb5ee072da2c43d7
Diego <diegoubatuba103@gmail.com>
2025-02-21 20:24:50 -0300

    anatomia-das-classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    public static void main(String[] args){
+        System.out.println("ola, mundo!");
+
+        String primeiroNome = "Diego";
+        String segundoNome = "Araújo";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome + " " + segundoNome;
+    }
+}
