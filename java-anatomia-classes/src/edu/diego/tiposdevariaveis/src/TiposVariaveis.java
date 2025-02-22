014e862aae4beae7b52da7cc8251cc9e787e7afb
Diego <diegoubatuba103@gmail.com>
2025-02-22 10:44:18 -0300

    tipos de variaveis

--- java-anatomia-classes/src/edu/diego/tiposdevariaveis/src/TiposVariaveis.java
+++ java-anatomia-classes/src/edu/diego/tiposdevariaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         double salarioMinimo = 2500; // double é um tipo de variável que aceita números decimais//
+
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal; // casting
+
+         final double valor_de_pi = 3.14; // final é uma constante
     }
 }
